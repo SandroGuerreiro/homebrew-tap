0829f31c7f0eae63fa9f3d16b3921fd0718db55c
Sandro Guerreiro <sandrocguerreiro@gmail.com>
2026-02-24 15:55:39 +0100

    Update pronto cask to v0.2.1

--- Casks/pronto.rb
+++ Casks/pronto.rb
@@ -1,6 +1,6 @@
 cask "pronto" do
-  version "0.2.0"
-  sha256 "7935ed7b4f67c74e8cbff2b2a5f7130b6f34736b20e9cc9770e8f91328dd3651"
+  version "0.2.1"
+  sha256 "1f2e8d0dc7d6dd14b3d4eaa95ab78de08db6028df78dc70e815158c2a7233131"
 
   url "https://github.com/SandroGuerreiro/pronto/releases/download/v#{version}/Pronto_#{version}_aarch64.dmg"
   name "Pronto"
