e21901f90832cf1da8d61530ee4658bedc2195a7
Sandro Guerreiro <sandrocguerreiro@gmail.com>
2026-02-25 11:57:19 +0100

    Update pronto to 0.4.1

--- Casks/pronto.rb
+++ Casks/pronto.rb
@@ -1,6 +1,6 @@
 cask "pronto" do
-  version "0.4.0"
-  sha256 "a03dfc0e6e6bfd6c1dda930ca127013b2796e00256a17b10414762a56928ccf8"
+  version "0.4.1"
+  sha256 "a095fb493936c6eb6871e44e3f672f82af58d655bedc9b2af76a1b38185de83a"
 
   url "https://github.com/SandroGuerreiro/pronto/releases/download/v#{version}/Pronto_#{version}_aarch64.dmg"
   name "Pronto"
